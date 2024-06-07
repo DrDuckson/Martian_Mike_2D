b8be481b41ae2b6d65dc2a651b18f866e4698840
DrDuckson <gorealex95@web.de>
2024-06-08 00:23:50 +0200

    Added movement with animation switch

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,39 @@
 extends CharacterBody2D
+class_name Player
 
 @onready var animated_sprite = $AnimatedSprite2D
 @export var gravity = 400
-@export var speed = 20
+@export var speed = 130
+@export var jump_force = 200
+
 func _physics_process(delta):
-	if is_on_floor() == false:
+	if !is_on_floor():
 		velocity.y += gravity*delta
-		
-	# nochmal überarbeiten!!!
-	if Input.is_action_pressed("move_left"):
-		velocity.x = -speed*delta
-	if Input.is_action_just_pressed("move_right"):
-		velocity.x = +speed*delta
-	if Input.is_action_just_pressed("jump"):
-		pass
+		if velocity.y > 500:
+			velocity.y = 500
+			
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		#velocity.y = - jump_force
+		jump(jump_force)
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction != 0:
+		animated_sprite.flip_h = (direction == -1)
+	velocity.x = direction*speed
 	move_and_slide()
+	update_animations(direction)
+	
+func jump(force):
+	velocity.y = -force
+	
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")

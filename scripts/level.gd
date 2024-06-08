2892b718dba72622132dcc6b739cf418e3c81668
DrDuckson <gorealex95@web.de>
2024-06-09 01:01:16 +0200

    Added groups for player and traps

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
-@onready var start_position = $StartPosition
-@onready var player = $Player
+@onready var start_zone = $StartZone
+var player = null
+
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+	if player != null:
+		player.global_position = start_zone.get_spawn_pos()
+	var traps = get_tree().get_nodes_in_group("traps")
+	for trapIndex in traps:
+		#alte methode für "connect signal"
+		#trapIndex.connect("touched_player", _on_trap_touched_player)	
+		#neue methode
+		trapIndex.touched_player.connect(_on_trap_touched_player)
 
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
@@ -17,4 +28,4 @@ func _on_trap_touched_player():
 
 func reset_player():
 	player.velocity = Vector2.ZERO
-	player.global_position = start_position.global_position
+	player.global_position = start_zone.get_spawn_pos()

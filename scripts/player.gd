12cfb92db7ec5f0f2c0c28aa4cf16999d9a9c550
DrDuckson <gorealex95@web.de>
2024-06-10 23:49:23 +0200

    Edited movement to stop if entered exit

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,17 +6,18 @@ class_name Player
 @export var speed = 130
 @export var jump_force = 200
 
+var active = true
+
 func _physics_process(delta):
 	if !is_on_floor():
 		velocity.y += gravity*delta
 		if velocity.y > 500:
 			velocity.y = 500
-			
-	if Input.is_action_just_pressed("jump") && is_on_floor():
-		#velocity.y = - jump_force
-		jump(jump_force)
-	
-	var direction = Input.get_axis("move_left", "move_right")
+	var direction = 0
+	if active == true:
+		if Input.is_action_just_pressed("jump") && is_on_floor():
+			jump(jump_force)
+		direction = Input.get_axis("move_left", "move_right")
 	if direction != 0:
 		animated_sprite.flip_h = (direction == -1)
 	velocity.x = direction*speed

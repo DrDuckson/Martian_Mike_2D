d86c6932133fb347a7647fd677b7fa34171cd857
DrDuckson <gorealex95@web.de>
2024-06-10 23:49:09 +0200

    Added timer function and edited exit-entered function

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,7 +1,20 @@
 extends Node2D
 
+@export var next_level: PackedScene = null
+@export var level_time = 60
+@export var is_final_level: bool = false
+
 @onready var start_zone = $StartZone
+@onready var exit = $Exit
+@onready var deathzone = $Deathzone
+@onready var hud = $UI_Layer/HUD
+@onready var ui_layer = $UI_Layer
+
+
 var player = null
+var timer_node = null
+var time_left
+var win = false
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
@@ -13,6 +26,28 @@ func _ready():
 		#trapIndex.connect("touched_player", _on_trap_touched_player)	
 		#neue methode
 		trapIndex.touched_player.connect(_on_trap_touched_player)
+	exit.body_entered.connect(_on_exit_body_entered)
+	deathzone.body_entered.connect(_on_deathzone_body_entered)
+	levelTimer()
+	
+func levelTimer():
+	time_left = level_time
+	hud.set_time_label(time_left)
+	timer_node = Timer.new()
+	timer_node.name = "Level_Timer"
+	timer_node.wait_time = 1
+	timer_node.timeout.connect(_on_level_timer_timeout)
+	add_child(timer_node)
+	timer_node.start()
+	
+func _on_level_timer_timeout():
+	if win == false:
+		time_left -= 1
+		hud.set_time_label(time_left)
+		if time_left < 0:
+			reset_player()
+			time_left = level_time
+			hud.set_time_label(time_left)
 
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
@@ -29,3 +64,15 @@ func _on_trap_touched_player():
 func reset_player():
 	player.velocity = Vector2.ZERO
 	player.global_position = start_zone.get_spawn_pos()
+
+func _on_exit_body_entered(body):
+	if body is Player:
+		if is_final_level || (next_level != null):
+			exit.animate()
+			player.active = false
+			win = true
+			await get_tree().create_timer(2).timeout
+			if is_final_level:
+				ui_layer.show_win_screen(true)
+			else:
+				get_tree().change_scene_to_packed(next_level)
